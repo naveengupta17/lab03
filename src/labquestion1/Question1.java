ea71877c0b21276b095241532429d00249741c4d
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-08-31 17:34:09 +0530

    adding Question2.java

--- src/labquestion1/Question1.java
+++ src/labquestion1/Question1.java
@@ -1,18 +1,1 @@
-package labquestion1;
 
-import java.util.Scanner;
-
-public class Question1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double n = scanner.nextInt();
-        double s = 0.0;
-        for (double i = 1; i <= n; i++) {
-            s = s + 1 / i;
-
-        }
-        System.out.println(s);
-
-
-    }
-}
