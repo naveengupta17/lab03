255daf4e6103a2c16246d43f374b97de3986a5b4
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-09-01 18:48:35 +0530

    calculating output

--- src/labquestion1/Question2.java
+++ src/labquestion1/Question2.java
@@ -1,8 +1,21 @@
 package labquestion1;
 
+import java.util.Scanner;
+
 public class Question2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double price = scanner.nextDouble();
+        int rupee = 0;
+        int paise = 0;
+        rupee = (int) price;
+        price = price * 100;
+        paise = (int) price % 100;
+        System.out.println(rupee + " Rupees and " + paise + "paise");
 
     }
 
+
 }
+
+
